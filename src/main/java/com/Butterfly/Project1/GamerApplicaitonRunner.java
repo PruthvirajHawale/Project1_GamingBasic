57f61ec46f5c053f8856a3cda523d5745afd5c23
PruthvirajHawale <hawalepruthvi12@gmail.com>
2023-09-09 18:37:55 +0530

    Removed the configuration file and Added bean creation to Apprunner and made it configuration

--- src/main/java/com/Butterfly/Project1/GamerApplicaitonRunner.java
+++ src/main/java/com/Butterfly/Project1/GamerApplicaitonRunner.java
@@ -1,6 +1,7 @@
 package com.Butterfly.Project1;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import com.Butterfly.Project1.game.GameRunner;
 import com.Butterfly.Project1.game.GamingConsole;
@@ -10,11 +11,23 @@ import com.Butterfly.Project1.game.SuperContra;
 
 //Introduced configuration class and created spring context for the games 
 //Creating objects in spring context by self -- need to improve
-public class App03GamingBasicJava2 {
+public class GamerApplicaitonRunner {
+	
+	@Bean
+	public GamingConsole game() {
+		return new SuperContra(); //Creating Object by our own
+	}
+	
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {
+		var gameRunner = new GameRunner(game);
+		return gameRunner;
+	}
+	
 	public static void main(String[] args) {
 		try( var context = 
 				new AnnotationConfigApplicationContext(
-						GameConfiguration.class);){
+						GamerApplicaitonRunner.class);){
 		context.getBean(GameRunner.class).run();
 		}
 	}
--- src/main/java/com/Butterfly/Project1/game/SuperContra.java
+++ src/main/java/com/Butterfly/Project1/game/SuperContra.java
@@ -15,3 +15,4 @@ public class SuperContra implements GamingConsole{
 	}
 
 }
+	
