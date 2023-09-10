92f18ec9449843c18f3e567218af7e07418ed459
PruthvirajHawale <hawalepruthvi12@gmail.com>
2023-09-10 15:01:05 +0530

    Lazy Initialization Implementes

--- src/main/java/com/Butterfly/Project1/Exercise001/BusinessCalculationService.java
+++ src/main/java/com/Butterfly/Project1/Exercise001/BusinessCalculationService.java
@@ -8,32 +8,42 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
 
 
+
+@Component
+@Lazy
+class A{
+	
+}
+
+@Component
+class B{
+	A a;
+	
+	public B(A a) {
+		System.out.println("Bean Initialized ....... ");
+		this.a = a;
+	}
+}
 
 @Configuration
 @ComponentScan
 public class BusinessCalculationService {
 	
-	@Autowired
-	@Qualifier("main")
-	DataService dataService;
-	
-//	public BusinessCalculationService(DataService dataService) {
-//		this.dataService = dataService;
-//	}
-//	
-	public int findmax() {
-		return Arrays.stream(dataService.retrieveData()).max().orElse(0);
-	}
-		
 	public static void main(String[] args) {
+		
+		System.out.println("Spring context being created .....");
 		try( var context = 
 				new AnnotationConfigApplicationContext(
 						BusinessCalculationService.class);){
+		System.out.println("Spring context Created ");
 		
-			System.out.println(context.getBean(BusinessCalculationService.class).findmax());
+		
+			System.out.println(context.getBean(A.class));
 
 		}
 	}
---
+++ src/main/java/com/Butterfly/Project1/examples/LazyInitializationLauchApp/SimpleSpringContextLauncherApplication.java
@@ -0,0 +1,56 @@
+package com.Butterfly.Project1.examples.LazyInitializationLauchApp;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class YourBusinessClass {
+
+	Dependency1 d1;
+
+	Dependency2 d2;
+
+
+	@Autowired
+	public YourBusinessClass(Dependency1 d1, Dependency2 d2) {
+		super();
+		this.d1 = d1;
+		this.d2 = d2;
+	}
+
+	public String toString() {
+		System.out.println("Setter Injection - setDependency2 ");
+		return "Using " + d1 + " and " + d2 + "Dependencies";
+	}
+
+}
+
+@Configuration
+class Dependency1 {
+
+}
+
+@Configuration
+class Dependency2 {
+
+}
+
+@Configuration
+@ComponentScan
+public class SimpleSpringContextLauncherApplication {
+
+	public static void main(String[] args) {
+		try (var context = new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class);) {
+
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+			System.out.println(context.getBean(YourBusinessClass.class));
+		}
+	}
+
+}
